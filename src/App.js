fba6614df3f5f593e73e87a7ed5bcc918b11040e
prabhdeep kaur <prabhdeep1234kaur@gmail.com>
2025-01-08 23:40:35 +0530

    detection functions added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@
  * setup webcam : done
  * define references : done
  * load facemesh: done
- * 
+ * detect functions: done
  * 
  */
 
@@ -24,9 +24,37 @@ function App() {
   const runFacemesh = async() =>{
     const net = await facemesh.load({
       inputResolution: {width: 640, height: 480}, scale: 0.8 //how big of a photo will be loaded
-    })
+    });
+    setInterval(()=>{
+      detect(net)
+    }, 100) //every 100 milliseconds, detecting our face
+  };
+
+   //detect function: runs when app starts and detect the model
+  const detect = async(net) =>{
+    if(typeof webcamRef.current !== "undefined" 
+    && webcamRef.current !== null 
+    && webcamRef.current.video.readyState === 4){ //webcam is up and running + receiving data
+      //get video properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+      //set video and canvas width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      //make detections: network from tensorflow as net: basically from webcam
+      const face = await net.estimateFaces(video);
+      console.log(face);
+      //get canvas content for drawing
+
+
+    }
   }
 
+  runFacemesh();//trigger the function
 
   return ( 
 
